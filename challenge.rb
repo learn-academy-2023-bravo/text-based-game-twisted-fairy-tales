da05ac7973de2514e1f3f7deff6097cfc291f545
bjohnwell <brandonjohnwell@gmail.com>
2023-04-10 12:03:30 -0700

    Planning done working on code. Do we need a def to call options 1 and 2?

--- challenge.rb
+++ challenge.rb
@@ -1,1 +1,57 @@
 # Text-based Game Challenge
+# start
+# prompt "choose to be the hero or villain"
+    # 1 Choices:(STRETCH)
+    #     Option 1 = villain(DEFAULT)
+    #     Option 2 = hero
+        # User selects option 1:
+        # 2 Choices:(STRETCH)
+        # Option 1 = little_red_riding_hood(LRR)(DEFAULT)
+        # Option 2 = three_little_pigs(STRETCH)
+            # User selects Option 1:
+            # 3 choices:
+            # Option 1 = Go_to_little_red
+            # Option 2 = Go_to_grandmas_house
+                # User selects Option 1:
+                # 4 choices:
+                # Option 1 = Eat_little_red
+                # Option 2 = Distract little red and go to grandmas house
+                    # User chooses option 1:
+                    # 5 choices:
+                    # Option 1: Sleep after eating little read in a single bite
+                    # Option 2: You're still hungry and go to grandmas house to eat her too.
+                        # User chooses option 1:
+                        # "while you sleep the hunts man approaches you and hears little red in your stomach. he slices you open freeing little red."
+            # Choice #3a User chooses option 2:
+            # Option 1 = eat grandma
+            # Option 2 = convince grandma a nigerian prince needs her money
+                # user chooses option 1
+                # choices:
+                # Option 1 = Leave and hunt little red
+                # Option 2 = dress as grandma and wait for little red
+                    # User Chooses Option 1
+                    # "As soon as you open the door you see the hunts man standing there. he slices you open and frees grandma"      
+            # User chooses Option 2 = convince grandma a nigerian prince needs her money
+            # choices:
+            # Option 1 hand grandma your phone to wire the money to the prince
+            # Option 2 wait for grandma to find her checkbook.
+                # user chooses option 1
+                # "grandma wires all the money from the wolfs bank account into her own and calls the huntsman"
+                # user chooses option 2
+                # "Grandma reaches benind her bed and pulls out an axe and slices the wolf up."
+
+
+def start_game
+   welcome_message =  "You're the big bad wolf and your goal is to eat"
+end
+puts start_game
+#  last thing we talked about* do we need a def to call option 1 and 2
+        option1 = "Go distract little red to delay her from grandmas house"
+        option2 = "Go directly to grandmas house"
+
+ def story_prompt
+    promt = "you see little red riding hood heading to grandmas house what do you do"
+       
+end
+
+first_choice = gets.chomp 
